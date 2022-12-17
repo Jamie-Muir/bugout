be077a96d8f0c35627bde0fee5ea74e8d5b1d014
jamie-muir <jamiemuircs@gmail.com>
2022-12-17 01:25:53 +0000

    Extract Table comp from project list

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,50 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
-import Card from '../UI/Card';
-import classes from './ProjectList.module.scss';
-
-const ProjectItem = (props) => {
-	return (
-		<tr>
-			<td>{props.id}</td>
-			<td>{props.name}</td>
-			<td>{props.issueCount}</td>
-			<td>{props.contributors}</td>
-			<td>{props.status}</td>
-			<td>
-				<Link to={`/projects/:${props.id}`}>
-					<button>
-						View Project
-					</button>
-				</Link>
-			</td>
-		</tr>
-	)
-}
+// import Card from '../UI/Card';
+import Table from '../UI/Table';
+// import classes from './ProjectList.module.scss';
 
 function ProjectList(props) {
-
-	const list = props?.projects.map(item => <ProjectItem key={item.id} {...item} />)
+	const projectHeadings = [
+		'Id',
+		'Project Name',
+		'Open Issues',
+		'Contributors',
+		'Status',
+	]
 
 	return (
-		<Card className={classes.container} title='Project List'>
-			<table>
-				<thead>
-					<tr>
-						<th>Id</th>
-						<th>Project Name</th>
-						<th>Open Issues</th>
-						<th>Contributors</th>
-						<th>Status</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					{list}
-				</tbody>
-			</table>
-		</Card>
+		<Table tableData={props.projects} tableHeadings={projectHeadings} />
 	)
 }
 
