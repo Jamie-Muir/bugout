23858fd4d01962815eb156655dbff6a8a311d241
jamie-muir <jamiemuircs@gmail.com>
2022-12-18 19:12:13 +0000

    read project data

--- src/components/details/ProjectDetails.js
+++ src/components/details/ProjectDetails.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import FakeTickets from '../../store/FakeTickets';
+
 import TicketList from '../tables/TicketList';
+import UserList from '../tables/UserList';
 import BackButton from '../UI/BackButton';
 import Card from '../UI/Card';
 
+import FakeProjects from '../../store/FakeProjects';
+import FakeTickets from '../../store/FakeTickets';
+import FakeUsers from '../../store/FakeUsers';
+
 function ProjectDetails(props) {
 	const params = useParams();
+	const project = FakeProjects.find(project => project.id == params.id);
 
 	return (
 		<>
 			<BackButton />
-			<Card title='Project Details'>
-				<p>Id: {params.id}</p>
+			<Card title={`${project?.name}`}>
+				<p>Status: {project.status} </p>
+				<p>Contributors: </p>
+				<ul>
+					{project.contributors.map(c => <li key={c}>{c}</li>)} 
+				</ul>
 				<footer />
 			</Card>
 			<TicketList tickets={FakeTickets} />
+			<UserList users={FakeUsers} />
 		</>
 	)
 }
