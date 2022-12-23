b74748e58d4fffe46dad9089cb9acc3af46461a1
jamie-muir <jamiemuircs@gmail.com>
2022-12-23 20:03:43 +0000

    title > heading, remove amiguity

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -5,7 +5,7 @@ import classes from './Card.module.scss';
 function Card(props) {
 	return (
 		<div className={`${classes.card} ${props.className}`}>
-			<h2>{props.title}</h2>
+			<h2>{props.heading}</h2>
 			{props.children}
 		</div>
 	)
