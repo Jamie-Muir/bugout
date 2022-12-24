46ea7f9917653827fc78d4312afc4bc9051dcaea
jamie-muir <jamiemuircs@gmail.com>
2022-12-24 00:36:50 +0000

    sidebar isActive attempt

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,34 +1,56 @@
 import React from 'react'
 import { FaHome, FaProjectDiagram, FaTicketAlt, FaUserFriends } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import classes from './Sidebar.module.scss';
 
 const iconSize = 30;
+
+let activeClassName = 'activeNav';
 
 function Sidebar() {
 	return (
 		<nav className={classes.sidebar}>
 			<ul>
 				<li>
-					<Link to='/'>
-						<FaHome size={iconSize} className={ classes.icon } />
-					</Link>
+					<NavLink
+						to='/'
+						className={({ isActive }) =>
+							isActive ? activeClassName : undefined
+						}
+					>
+						<FaHome size={iconSize} className={classes.icon} />
+					</NavLink>
 				</li>
 				<li>
-					<Link to='/projects'>
-						<FaProjectDiagram size={iconSize} className={ classes.icon } />
-					</Link>
+					<NavLink
+						to='/projects'
+						className={({ isActive }) =>
+							isActive ? activeClassName : undefined
+						}
+					>
+						<FaProjectDiagram size={iconSize} className={classes.icon} />
+					</NavLink>
 				</li>
 				<li>
-					<Link to='/tickets'>
-						<FaTicketAlt size={iconSize} className={ classes.icon } />
-					</Link>
+					<NavLink
+						to='/tickets'
+						className={({ isActive }) =>
+							isActive ? activeClassName : undefined
+						}
+					>
+						<FaTicketAlt size={iconSize} className={classes.icon} />
+					</NavLink>
 				</li>
 				<li>
-					<Link to='/users'>
-						<FaUserFriends size={iconSize} className={ classes.icon } />
-					</Link>
+					<NavLink
+						to='/users'
+						className={({ isActive }) =>
+							isActive ? activeClassName : undefined
+						}
+					>
+						<FaUserFriends size={iconSize} className={classes.icon} />
+					</NavLink>
 				</li>
 			</ul>
 		</nav>
