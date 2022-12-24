b565233cfb0195eb7538923e94591c2644b9b2b7
jamie-muir <jamiemuircs@gmail.com>
2022-12-24 00:39:32 +0000

    details, fetch from store has problems

--- src/components/details/ProjectDetails.js
+++ src/components/details/ProjectDetails.js
@@ -7,29 +7,34 @@ import TicketList from '../tables/TicketList';
 import UserList from '../tables/UserList';
 import Details from './Details';
 
-import LoadingSpinner from '../UI/LoadingSpinner';
+// import LoadingSpinner from '../UI/LoadingSpinner';
+import { useSelector } from 'react-redux';
 
 function ProjectDetails(props) {
 	const params = useParams();
+	let project = useSelector(state => state.projects?.list?.find(item => item.id === params.id))
 
-	const { sendRequest, status, data: project } = useHttp(getSingleEntry, true);
+	// const { sendRequest, status, data } = useHttp(getSingleEntry, true);
 
-	useEffect(() => {
-		sendRequest(params.id, 'projects')
-	},[sendRequest, params.id]);
-	
-	if(status === 'pending') {
-		return (
-			<div className='centered'>
-				<LoadingSpinner />
-			</div>
-		)
-	}
+	// useEffect(() => {
+	// 	if (!project) {
+	// 		sendRequest(params.id, 'projects')
+	// 		project = data;
+	// 	}
+	// }, [sendRequest, params.id, project]);
+
+	// if (status === 'pending') {
+	// 	return (
+	// 		<div className='centered'>
+	// 			<LoadingSpinner />
+	// 		</div>
+	// 	)
+	// }
 
 	if (!project) {
 		return <Details heading='Item Not found' id='null' />
 	}
-	
+
 	return (
 		<>
 			<Details heading={project.title} id={project.id}>
